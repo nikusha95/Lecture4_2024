41989312190ac31289d38ae5cd2aa15bf92d5b77
nikusha95 <nikasanaia11@yahoo.com>
2023-03-22 22:33:54 +0400

    init

---
+++ Conditions/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string str1 = Console.ReadLine();
+string str2 = Console.ReadLine();
+
+Console.WriteLine($"before parse {str1}, {str2}");
+
+bool x1 = int.TryParse(str1, out int result1);
+bool x2 = int.TryParse(str1, out int result2);
+
+
+if (x1)
+{
+    result1 += 1;
+    Console.WriteLine(result1); //good
+}
+else
+{
+    Console.WriteLine($"{str1} is not number");
+}
+
+if (x2)
+{
+    result2 += 1;
+    Console.WriteLine(result2); //good
+}
+else
+{
+    Console.WriteLine($"{str1} is not number");
+}
+
+if (x1)
+{
+    if (result1 is > 10 and < 20)//if (result1 > 10 && result1 < 20)
+    {
+        Console.WriteLine("result is between 10 an 20");
+    }
+    else if (result1 < 10)
+    {
+        Console.WriteLine("result is less than 10");
+    }
+    else
+    {
+        Console.WriteLine("result is greater than 20");
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Channels;
+
+string s = Console.ReadLine();
+
+
+bool res = int.TryParse(s, out int number);
+if (res)
+{
+    int even = 0, odd = 0;
+
+    while (number > 0)
+    {
+        if (number % 2 == 0)
+        {
+            even += number;
+        }
+        else
+        {
+            odd += number;
+        }
+
+        number--;
+    }
+
+    Console.WriteLine($"even sum is {even}\nodd sum is {odd}");
+}
+
+int x = 4;
+int sum = 0;
+while (x > 0)
+{
+    Console.WriteLine(x);
+    if (x == 3)
+    {
+        x--;
+        continue;
+    }
+
+    sum += x;
+
+    x--;
+}
+
+Console.WriteLine($"sum is {sum}");
+
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
